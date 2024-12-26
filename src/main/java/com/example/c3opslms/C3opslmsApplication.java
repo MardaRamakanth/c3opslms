a4fdcd77c74a33dbbcaf9845c146ac352ccd7b10
Marda Ramakanth <ramakanthmarda07@gmail.com>
2024-12-26 22:08:26 +0530

    Your commit message

---
+++ src/main/java/com/example/c3opslms/C3opslmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.c3opslms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3opslmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C3opslmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/c3opslms/C3opslmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c3opslms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C3opslmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
